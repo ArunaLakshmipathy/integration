7b1bf44748d8f905013e6128274bdff6c798e176
Aruna <arunapaari@gmail.com>
2023-02-22 19:58:00 +0530

    integration

---
+++ src/arun/aru.java
@@ -0,0 +1,39 @@
+package arun;
+
+public class aru {
+
+	public static void main (String[] args) {
+		
+		
+        int a = 100;
+if ( a<= 90) {
+	System.out.println(" false");
+	
+	
+}
+	
+if(a>= 90)
+{
+	System.out.println(" true");
+}
+}
+}
+int number=30;
+{
+
+
+for (int i = 2 ; i <=30; i++) 
+{
+
+
+	System.out.println("i=i++");
+
+	if(2% 2==0 )
+	{
+	System.out.println("i=i++");
+	
+}
+	if (3%2==1) {
+		System.out.println("i=i+1");
+	}
+}
